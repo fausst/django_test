55563a8c72748d65af1aadc75250464577347286
fausst <fausst90@gmail.com>
2019-12-21 20:06:06 +0100

    static

--- django/yourproject/settings/production.py
+++ django/yourproject/settings/production.py
@@ -4,7 +4,7 @@ from os import environ
 
 from .base import *
 
-ALLOWED_HOSTS = ['localhost','aws-elb-demo-1267616547.us-east-2.elb.amazonaws.com']
+ALLOWED_HOSTS = ['localhost']
 
 DATABASES = {
     'default': dj_database_url.config()
